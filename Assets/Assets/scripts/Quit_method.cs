1eb9eeca87adf672553015530ccdf74c80f49995
Aishwarya <vaishwarya703@gmail.com>
2024-08-04 22:33:11 +0530

    COMMIT

---
+++ Assets/Assets/scripts/Quit_method.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_method : MonoBehaviour
+{
+    public void quit(){
+        Application.Quit();
+    }
+}
